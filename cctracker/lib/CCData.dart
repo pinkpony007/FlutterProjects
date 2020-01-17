7ccab93cc2b3af713d3905f723bc7a45a10e6b00
pinkpony007 <danyllamanov@gmail.com>
2020-01-17 22:48:35 +0200

    Cryptocurrency rate tracking application

---
+++ cctracker/lib/CCData.dart
@@ -0,0 +1,9 @@
+class CCData{
+  String name;
+  String symbol;
+  int rank;
+  double price;
+
+  CCData({this.name, this.symbol, this.rank, this.price});
+
+}
---
+++ cctracker/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:cctracker/CCList.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(CCTracker());
+
+class CCTracker extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'CC Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: CCList(),
+    );
+  }
+
+}
