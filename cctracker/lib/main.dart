cb67dfed63c468a8e74c147bce2f492ef7b15d80
pinkpony007 <danyllamanov@gmail.com>
2020-01-20 23:45:03 +0200

    delete todo comment)

--- cctracker/lib/main.dart
+++ cctracker/lib/main.dart
@@ -7,7 +7,6 @@ void main() => runApp(CCTracker());
 class CCTracker extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'CC Tracker',
